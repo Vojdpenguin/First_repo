644186b6eccd3614b5e9aa0bb09b622401db7d02
Vojdpenguin <regush20@gmail.com>
2024-07-01 17:41:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-num = 15  # приклад значення для num
+from datetime import datetime
 
-if num > 10:
-    print("num більше за 10")
-else:
-    print("num не більше за 10")
+
+start_date = string_to_date("2024.03.26")  # Перетворення рядка на дату
+next_monday = find_next_weekday(start_date, 0)  # Знайти наступний понеділок
+print(next_monday)
+next_friday = find_next_weekday(start_date, 4)  # Знайти наступну п'ятницю
+print(next_friday)
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+def find_next_weekday(start_date, weekday):
+    day_of_week = start_date.weekday()
+
+
+
+print(None)
+    
+        
